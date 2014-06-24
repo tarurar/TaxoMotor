7b5adc9149cb3e83c8d411213d4a48e5a487a4fb
tarurar <tarutin_andrew@mail.ru>
2014-06-24 15:21:47 +0400

    SQL DB schema

---
+++ TM.Database/IncomingRequestXML.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[IncomingRequestXML]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1), 
+    [RequestBody] XML NOT NULL, 
+    [InDate] DATETIME NOT NULL, 
+    [Source] NVARCHAR(50) NULL,
+	[OutDate] DATETIME NULL, 
+    [ProcessLastAttemptDate] DATETIME NULL, 
+    [Processed] AS CAST(CASE ISNULL([OutDate], 0) WHEN 0 THEN 0 ELSE 1 END AS BIT) PERSISTED, 
+    [BodyMessageId] AS [dbo].[fn_GetCoordinateV5RequestMessageId](RequestBody),
+	CONSTRAINT IncomingRequestXML_UniqueMessageId UNIQUE(BodyMessageId)
+)
---
+++ TM.Database/fn_GetCoordinateV5RequestMessageId.sql
@@ -0,0 +1,18 @@
+﻿CREATE FUNCTION [dbo].[fn_GetCoordinateV5RequestMessageId]
+(
+	@messageXML xml
+)
+RETURNS VARCHAR(36)
+WITH SCHEMABINDING
+AS
+BEGIN
+	DECLARE @RETVALUE VARCHAR(36);
+
+	SET @RETVALUE = @messageXML.value('
+		declare namespace s="http://schemas.xmlsoap.org/soap/envelope/";
+		declare namespace h="http://asguf.mos.ru/rkis_gu/coordinate/v5/";
+		declare default element namespace "http://asguf.mos.ru/rkis_gu/coordinate/v5/";
+		/s:Envelope[1]/s:Header[1]/h:ServiceHeader[1]/MessageId[1]', 'varchar(36)');
+
+	RETURN @RETVALUE;
+END
