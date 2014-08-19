dfe7a55f9999401c168c4a52d0aa09a0ff20a6f2
tarurar <tarutin_andrew@mail.ru>
2014-08-19 13:56:41 +0400

    field names changes

---
+++ TM.Database/dbo/Tables/RequestMigrationTicket.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[RequestMigrationTicket]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
+	[Title] AS [MessageId],
+	[MessageId] VARCHAR(36) NOT NULL,
+	[Status] INT NOT NULL DEFAULT 0, 
+    [StartDate] DATETIME NULL, 
+    [FinishDate] DATETIME NULL, 
+    [ErrorInfo] NVARCHAR(MAX) NULL, 
+    [StackTrace] NVARCHAR(MAX) NULL,
+	[RequestId] INT NOT NULL,
+	CONSTRAINT [FK_RequestMigrationTicket_Request] FOREIGN KEY ([RequestId]) REFERENCES [dbo].[Request]([Id])
+)
