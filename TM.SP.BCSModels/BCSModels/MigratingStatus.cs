19be98f60e999dd4970682c5686b207c25188d95
tarurar <tarutin_andrew@mail.ru>
2014-10-15 16:40:49 +0400

    licenses migration bcs model support

---
+++ TM.SP.BCSModels/BCSModels/MigratingStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TM.SP.BCSModels
+{
+    public enum MigratingStatus
+    {
+        Undefined = 0,
+        Reserved,
+        Processing,
+        Processed,
+        Error
+    }
+}
