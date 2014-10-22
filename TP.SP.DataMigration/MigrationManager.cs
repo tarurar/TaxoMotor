37f6b88ee1957aea1ce38961220f67c4f255e4a5
tarurar <tarutin_andrew@mail.ru>
2014-10-22 20:15:34 +0400

    new project for data migration logic

---
+++ TP.SP.DataMigration/MigrationManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint;
+
+namespace TP.SP.DataMigration
+{
+    public static class MigrationManager
+    {
+        public static void ProcessAny<ItemType>(int maxCount = 0)
+        {
+            
+        }
+
+        public static void Process<ItemType, TicketType>(int id, string contentType, Func<SPWeb, ItemType, SPListItem> assignFields)
+        {
+            // TakeItemForMigration method - add parameter
+        }
+    }
+}
