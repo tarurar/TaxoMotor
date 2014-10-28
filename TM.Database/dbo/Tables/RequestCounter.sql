bcccedf61eef391902b4abeec6f45af1ea30aa3f
tarurar <tarutin_andrew@mail.ru>
2014-10-28 11:49:09 +0200

    request counter table

---
+++ TM.Database/dbo/Tables/RequestCounter.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[RequestCounter]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY (1, 1),
+	[Title] as [Id],
+	[Year] INT NOT NULL,
+	[ServiceCode] NVARCHAR(10) NOT NULL,
+	[CounterValue] INT NOT NULL
+)
