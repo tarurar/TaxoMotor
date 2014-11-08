409d090eb1f04ef03ff0a2f10cd012db85b0b390
tarurar <tarutin_andrew@mail.ru>
2014-11-08 18:14:58 +0200

    License table trigger for auto calculating RegNumber

---
+++ TM.Database/dbo/Triggers/AI_License.sql
@@ -0,0 +1,28 @@
+﻿CREATE TRIGGER [AI_License] ON [dbo].[License]
+AFTER INSERT
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	DECLARE @Id INT;
+
+	SELECT @Id = i.Id
+	FROM INSERTED i;
+
+	UPDATE [dbo].[License]
+	SET [RegNumber] = CAST((
+				SELECT TOP 1 IntRegNumber
+				FROM (
+					SELECT CAST(RegNumber AS INT) + 1 AS IntRegNumber
+					FROM [dbo].[License]
+					WHERE RegNumber IS NOT NULL
+					) ii
+				WHERE IntRegNumber NOT IN (
+						SELECT CAST(RegNumber AS INT)
+						FROM [dbo].[License]
+						WHERE RegNumber IS NOT NULL
+						)
+				ORDER BY IntRegNumber
+				) AS NVARCHAR(64))
+	WHERE Id = @Id;
+END
