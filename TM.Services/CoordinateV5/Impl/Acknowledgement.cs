ea12f80f6ac959bbb075191c27bda53858c948fa
tarurar <tarutin_andrew@mail.ru>
2015-04-10 18:53:03 +0300

    acknowledgements redirection to queue

--- TM.Services/CoordinateV5/Impl/Acknowledgement.cs
+++ TM.Services/CoordinateV5/Impl/Acknowledgement.cs
@@ -6,11 +6,7 @@ namespace TM.Services.CoordinateV5
     {
         public static void Acknowledgement(ErrorMessage request)
         {
-            var queueUrl = Properties.Settings.Default.InputMessageQueueUrl;
-
-            var binding   = new System.ServiceModel.BasicHttpBinding();
-            var address   = new System.ServiceModel.EndpointAddress(queueUrl);
-            var client = new CoordinateV5Client(binding, address);
+            var client = new CoordinateV5Client("InputQueue");
             try
             {
                 client.Acknowledgement(request.ServiceHeader, request.Error);
