715cb0f81db704bc93b2588ba2d9471e15ada13b
tarurar <tarutin_andrew@mail.ru>
2015-04-24 16:44:57 +0300

    ODOPM and MO timer jobs

---
+++ TM.SP.AppPages/Features/TaxoMotor_TaxoMotor_CommonTimerJobs/TaxoMotor_CommonTimerJobs.EventReceiver.cs
@@ -0,0 +1,168 @@
+// <copyright file="TaxoMotor_CommonTimerJobs.EventReceiver.cs" company="CompanyName">
+// Copyright CompanyName. All rights reserved.
+// </copyright>
+// <author>TAXOMOTOR\developer</author>
+// <date>2015-04-24 15:40:56Z</date>
+namespace TM.SP.AppPages
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Security.Permissions;
+    using System.Text;
+    using Microsoft.SharePoint;
+    using Microsoft.SharePoint.Security;
+    using Microsoft.SharePoint.Administration;
+    using Timers;
+
+    /// <summary>
+    /// TODO: Add comment to TaxoMotor_CommonTimerJobsEventReceiver
+    /// </summary>
+    [SharePointPermission(SecurityAction.InheritanceDemand, ObjectModel = true)]
+    public class TaxoMotor_CommonTimerJobsEventReceiver : SPFeatureReceiver
+    {
+        private static readonly string OdopmJobName = "TaxoMotorOdopmJob";
+        private static readonly string MoJobName = "TaxoMotorMoJob";
+
+        public bool DeleteExistingJob(string jobName, SPWebApplication site)
+        {
+            bool jobDeleted = false;
+            try
+            {
+                foreach (SPJobDefinition job in site.JobDefinitions)
+                {
+                    if (job.Name == jobName)
+                    {
+                        job.Delete();
+                        jobDeleted = true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(String.Format("Couldn't delete timer job definition for {0}. Details: {1}", jobName, ex.Message));
+            }
+            return jobDeleted;
+        }
+        /// <summary>
+        /// TODO: Add comment to describe the actions after feature activation
+        /// </summary>
+        /// <param name="properties">Properties of the feature</param>
+        [SharePointPermission(SecurityAction.LinkDemand, ObjectModel = true)]
+        public override void FeatureActivated(SPFeatureReceiverProperties properties)
+        {
+            try
+            {
+                SPSecurity.RunWithElevatedPrivileges(delegate()
+                {
+                    SPWebApplication webApp = (SPWebApplication)properties.Feature.Parent;
+                    if (webApp != null)
+                    {
+                        DeleteExistingJob(OdopmJobName, webApp);
+                        DeleteExistingJob(MoJobName, webApp);
+
+                        try
+                        {
+                            OdopmTimer job = new OdopmTimer(OdopmJobName, webApp);
+                            SPDailySchedule schedule = new SPDailySchedule
+                            {
+                                BeginHour = 1,
+                                BeginMinute = 0,
+                                EndHour = 1,
+                                EndMinute = 15
+                            };
+                            job.Schedule = schedule;
+                            job.Update();
+                        }
+                        catch (Exception ex)
+                        {
+                            throw new Exception(String.Format("Couldn't create timer job definition for {0}. Details: {1}", OdopmJobName, ex.Message));
+                        }
+
+                        try
+                        {
+                            MoTimer job = new MoTimer(MoJobName, webApp);
+                            SPDailySchedule schedule = new SPDailySchedule
+                            {
+                                BeginHour = 4,
+                                BeginMinute = 0,
+                                EndHour = 4,
+                                EndMinute = 15
+                            };
+                            job.Schedule = schedule;
+                            job.Update();
+                        }
+                        catch (Exception ex)
+                        {
+                            throw new Exception(String.Format("Couldn't create timer job definition for {0}. Details: {1}", MoJobName, ex.Message));
+                        }
+                    }
+                });
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// TODO: Add comment to describe the actions during feature deactivation
+        /// </summary>
+        /// <param name="properties">Properties of the feature</param>
+        [SharePointPermission(SecurityAction.LinkDemand, ObjectModel = true)]
+        public override void FeatureDeactivating(SPFeatureReceiverProperties properties)
+        {
+            lock (this)
+            {
+                try
+                {
+                    SPSecurity.RunWithElevatedPrivileges(delegate()
+                    {
+                        SPWebApplication webApp = (SPWebApplication)properties.Feature.Parent;
+                        if (webApp != null)
+                        {
+                            DeleteExistingJob(OdopmJobName, webApp);
+                            DeleteExistingJob(MoJobName, webApp);
+                        }
+                    });
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+
+        /// <summary>
+        /// TODO: Add comment to describe the actions after feature installation
+        /// </summary>
+        /// <param name="properties">Properties of the feature</param>
+        [SharePointPermission(SecurityAction.LinkDemand, ObjectModel = true)]
+        public override void FeatureInstalled(SPFeatureReceiverProperties properties)
+        {
+            ////TODO: place receiver code here or remove method
+        }
+
+        /// <summary>
+        /// TODO: Add comment to describe the actions during feature uninstalling
+        /// </summary>
+        /// <param name="properties">Properties of the feature</param>
+        [SharePointPermission(SecurityAction.LinkDemand, ObjectModel = true)]
+        public override void FeatureUninstalling(SPFeatureReceiverProperties properties)
+        {
+            ////TODO: place receiver code here or remove method
+        }
+
+        /// <summary>
+        /// TODO: Add comment to describe the actions during feature upgrading
+        /// </summary>
+        /// <param name="properties">Properties of the feature</param>
+        /// <param name="upgradeActionName">The name of the custom upgrade action to execute. The value can be null if the override of this method implements only one upgrade action.</param>
+        /// <param name="parameters">Parameter names and values for the custom action</param>
+        [SharePointPermission(SecurityAction.LinkDemand, ObjectModel = true)]
+        public override void FeatureUpgrading(SPFeatureReceiverProperties properties, string upgradeActionName, System.Collections.Generic.IDictionary<string, string> parameters)
+        {
+            ////TODO: place receiver code here or remove method
+        }
+    }
+}
+
---
+++ TM.SP.AppPages/Timers/OdopmTimer.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.SharePoint;
+using Microsoft.SharePoint.Administration;
+using Microsoft.SharePoint.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using TM.Utils;
+
+namespace TM.SP.AppPages.Timers
+{
+    public class OdopmTimer: SPJobDefinition
+    {
+        #region [resource strings]
+
+        public static readonly string TaxiListsFeatureId = "{fd2daa37-e95d-4e98-b360-2f8390c3f2ba}";
+        public static readonly string TaxiV2ListsFeatureId = "{38cd390b-fda5-434c-8f3b-2810dee6c8a1}";
+
+        #endregion
+
+        #region [methods]
+        public OdopmTimer()
+        {}
+
+        public OdopmTimer(string jobName, SPService service): base(jobName, service, null, SPJobLockType.None)
+        {
+            Title = "ТаксоМотор: Отправка в ОДОПМ";
+        }
+
+        public OdopmTimer(string jobName, SPWebApplication webapp)
+            : base(jobName, webapp, null, SPJobLockType.Job)
+        {
+            Title = "ТаксоМотор: Отправка в ОДОПМ";
+        }
+
+        public override void Execute(Guid targetInstanceId)
+        {
+            try
+            {
+                var webApp = Parent as SPWebApplication;
+                if (webApp != null)
+                {
+                    foreach (SPSite siteCollection in webApp.Sites)
+                    {
+                        try
+                        {
+                            SPWeb web = siteCollection.RootWeb;
+
+                            if (web.Features[new Guid(TaxiListsFeatureId)] != null &&
+                                web.Features[new Guid(TaxiV2ListsFeatureId)] != null)
+                            {
+                                Work(web);
+                            }
+                        }
+                        finally
+                        {
+                            siteCollection.Dispose();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+	        {
+                throw new Exception(String.Format("Во время отправки данных в ОДОПМ произошла ошибка. Детали: {0}", ex.Message));
+	        }
+        }
+
+        private void Work(SPWeb web)
+        {
+            var cfg = Config.GetConfigValueOrDefault<string>(web, "AutoSendOdopm");
+            if (!String.IsNullOrEmpty(cfg))
+            {
+                if (cfg.Equals("1") || cfg.ToUpper().Equals("ДА"))
+                {
+                    var url = SPUtility.ConcatUrls(SPUtility.GetWebLayoutsFolder(web), "TaxoMotor/CommonService.aspx/SendOdopm");
+                    var request = WebRequest.Create(url);
+
+                    request.Method = "POST";
+                    request.ContentLength = 0;
+                    request.UseDefaultCredentials = true;
+                    var response = (HttpWebResponse)request.GetResponse();
+
+                    if (response.StatusCode != HttpStatusCode.OK)
+                        throw new Exception(String.Format("Error sending web request (url = {0})", url));
+                }
+            }
+        }
+
+        #endregion
+    }
+}
