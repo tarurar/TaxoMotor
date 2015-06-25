2f9836b0b15ca6786fac8f915593e8b62dfe303b
tarurar <tarutin_andrew@mail.ru>
2015-06-25 17:06:49 +0300

    fixed: determing acting licenses

--- TM.Database/dbo/Views/LicenseActing.sql
+++ TM.Database/dbo/Views/LicenseActing.sql
@@ -94,6 +94,5 @@ SELECT [L1].[Id]
 FROM [dbo].[License] AS L1
 LEFT JOIN [dbo].[License] L2 ON L1.Id = L2.Parent AND L2.STATUS <> 4
 WHERE L2.Id IS NULL -- last record
-	AND L1.TillSuspensionDate IS NULL -- not paused
 	AND L1.STATUS <> 3 -- not cancelled
 	AND L1.STATUS <> 4 -- no drafts
